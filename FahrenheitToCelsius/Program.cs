942f59b36ac63242ae7c8772b7af6289b95155d7
Magda <magdagvirjishvili@gmail.com>
2023-07-10 00:29:11 +0400

    magdas_commit

---
+++ FahrenheitToCelsius/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a temperature in Fahrenheit ");
+        string input = Console.ReadLine();
+        double fahr;
+
+        if (double.TryParse(input, out fahr))
+            {
+            double cels = 5.0 / 9.0 * (fahr - 32.0);
+            Console.WriteLine("Equivalent temperature in Celsius is " + cels);
+        }
+        else
+        {
+            Console.WriteLine("Conversion failed. Invalid input, enter correct value.");
+        }
+
+        
+    }
+}
